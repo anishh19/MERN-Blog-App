f0fcdbed247f9fabe1c1dcbbc07c74a943b96830
Anish <65701889+anishh19@users.noreply.github.com>
2023-04-10 23:57:12 +0530

    basic express app setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.port || 5000;
+
+const app = express();
+
+app.use("/api", require("./routes/blogPosts"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
