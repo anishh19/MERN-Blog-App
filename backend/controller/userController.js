791bc410a8e4a6d5c13ceb58ebececca8569e242
Anish <65701889+anishh19@users.noreply.github.com>
2023-04-20 11:03:38 +0530

    register user function error handling fixed

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,40 +2,41 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-const { use } = require("../routes/blogPosts");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     res.status(400).json({ message: "Enter all data" });
-  }
-  //check if user exists
-  const userExists = User.findOne({ email });
-  if (userExists) {
-    res.status(400);
-    throw new Error({ message: "User Already Exists" });
-  }
-
-  //hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const user = await User.create({
-    name,
-    email,
-    hashedPassword,
-  });
-
-  if (user) {
-    res.status(201).json({
-      _id: user.ID,
-      name: user.name,
-      email: user.email,
-    });
   } else {
-    res.status(400);
-    throw new Error({
-      message: "Invalid User Data",
-    });
+    //check if user exists
+    const userExists = await User.findOne({ email: email });
+    if (userExists) {
+      res.status(400);
+      throw new Error((message = "User Already Exists"));
+    } else {
+      //hash password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const user = await User.create({
+        name: name,
+        email: email,
+        password: hashedPassword,
+      });
+
+      if (user) {
+        res.status(201).json({
+          _id: user.ID,
+          name: user.name,
+          email: user.email,
+        });
+      } else {
+        res.status(400);
+        throw new Error({
+          message: "Invalid User Data",
+        });
+      }
+    }
   }
 });
 
