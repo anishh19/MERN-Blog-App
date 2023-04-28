e8661f6ea18f9643868589a564f55483df6b0f5f
Anish <65701889+anishh19@users.noreply.github.com>
2023-04-29 02:25:45 +0530

    form validation completed

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,21 +1,100 @@
 import Header from ".@component/components/header";
+import { z } from "zod";
+import { useFormik } from "formik";
+
+const LoginFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email is required!" })
+    .email({ message: "Invalid email address!" }),
+  password: z
+    .string()
+    .min(1, { message: "Password is required!" })
+    .min(8, { message: "Password should be atleast 8 characters long!" }),
+});
+
 function login() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    initialErrors: {
+      email: "",
+      password: "",
+    },
+    validate: (values) => {
+      let errors = {};
+
+      try {
+        LoginFormSchema.shape.email.parse(values.email);
+      } catch (err) {
+        const errorMessage = err.issues[0].message;
+        errors.email = errorMessage;
+      }
+      try {
+        LoginFormSchema.shape.password.parse(values.password);
+      } catch (err) {
+        const errorMessage = err.issues[0].message;
+        errors.password = errorMessage;
+      }
+
+      if (errors) return errors;
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div className="h-full w-screem">
       <Header currentTab="login" />
       <div className=" w-full flex flex-col items-center justify-center gap-4 py-8">
         <h1 className="text-6xl py-12">WELCOME </h1>
-        <form className="flex text-2xl flex-col gap-4 w-[80]">
+        <form
+          className="flex text-2xl flex-col gap-4 w-[80]"
+          onSubmit={formik.handleSubmit}
+        >
           <div className="flex flex-col">
-            <label>Email</label>
-            <input className="border-2 border-black p-2"></input>
+            <label className="m-2">Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className="border-2 border-black p-2"
+            ></input>
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-red-500 text-sm m-1">
+                {formik.errors.email}
+              </div>
+            ) : null}
           </div>
           <div className="flex flex-col">
-            <label>Password</label>
-            <input className="border-2 border-black p-2"></input>
+            <label className="m-2">Password</label>
+            <input
+              className="border-2 border-black p-2"
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+            ></input>
+            {formik.touched.password && formik.errors.password ? (
+              <div className="text-red-500 text-sm m-1">
+                {formik.errors.password}
+              </div>
+            ) : null}
           </div>
 
-          <button className="h-16 my-4 bg-slate-600 rounded-md text-white">
+          <button
+            type="submit"
+            className="h-16 my-4 bg-slate-600 rounded-md text-white"
+          >
             Login
           </button>
         </form>
--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,30 +1,156 @@
 import Header from ".@component/components/header";
+import { useState } from "react";
+import { z } from "zod";
+import { useFormik } from "formik";
+
+const LoginFormSchema = z.object({
+  name: z.string().min(1, { message: "Name is required!" }),
+  email: z
+    .string()
+    .min(1, { message: "Email is required!" })
+    .email({ message: "Invalid email address!" }),
+  password: z
+    .string()
+    .min(1, { message: "Password is required!" })
+    .min(8, { message: "Password should be atleast 8 characters long!" }),
+  repeatPassword: z.string().min(1, { message: "Password is required!" }),
+});
 
 function Signup() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      repeatPassword: "",
+    },
+    initialErrors: {
+      name: "",
+      email: "",
+      password: "",
+      repeatPassword: "",
+    },
+    validate: (values) => {
+      let errors = {};
+
+      try {
+        LoginFormSchema.shape.email.parse(values.email);
+      } catch (err) {
+        const errorMessage = err.issues[0].message;
+        errors.email = errorMessage;
+      }
+
+      try {
+        LoginFormSchema.shape.name.parse(values.name);
+      } catch (err) {
+        const errorMessage = err.issues[0].message;
+        errors.name = errorMessage;
+      }
+
+      try {
+        LoginFormSchema.shape.password.parse(values.password);
+      } catch (err) {
+        const errorMessage = err.issues[0].message;
+        errors.password = errorMessage;
+      }
+      if (values.password != values.repeatPassword) {
+        errors.repeatPassword = "Passwords do not match";
+      }
+      if (errors) return errors;
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div className="h-full w-screem">
       <Header currentTab="signup" />
 
-      <div className=" w-full flex flex-col items-center justify-center gap-4">
-        <h1 className="text-6xl py-12">WELCOME </h1>
-        <form className="flex text-2xl flex-col gap-3 w-[80]">
+      <div className=" w-full flex flex-col items-center justify-center gap-2">
+        <h1 className="text-4xl py-4">WELCOME </h1>
+        <form
+          className="flex text-xl flex-col w-[80] gap-1"
+          onSubmit={formik.handleSubmit}
+        >
           <div className="flex flex-col">
-            <label>Name</label>
-            <input className="border-2 border-black p-2"></input>
+            <label className="m-2">Name</label>
+            <input
+              className="border-2 border-black p-2 w-96"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter your name"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.name}
+            ></input>
+            {formik.touched.name && formik.errors.name ? (
+              <div className="text-red-500 text-sm m-1">
+                {formik.errors.name}
+              </div>
+            ) : null}
           </div>
           <div className="flex flex-col">
-            <label>Email</label>
-            <input className="border-2 border-black p-2"></input>
+            <label className="m-2">Email</label>
+            <input
+              className="border-2 border-black p-2"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter email address"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            ></input>
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-red-500 text-sm m-1">
+                {formik.errors.email}
+              </div>
+            ) : null}
           </div>
+
           <div className="flex flex-col">
-            <label>Password</label>
-            <input className="border-2 border-black p-2"></input>
+            <label className="m-2">Password</label>
+            <input
+              className="border-2 border-black p-2"
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+            ></input>
+            {formik.touched.password && formik.errors.password ? (
+              <div className="text-red-500 text-sm m-1">
+                {formik.errors.password}
+              </div>
+            ) : null}
           </div>
+
           <div className="flex flex-col">
-            <label>Confirm Password</label>
-            <input className="border-2 border-black p-2"></input>
+            <label className="m-2">Confirm Password</label>
+            <input
+              className="border-2 border-black p-2"
+              type="password"
+              id="repeatPassword"
+              name="repeatPassword"
+              placeholder="Enter password again"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.repeatPassword}
+            ></input>
+            {formik.touched.repeatPassword && formik.errors.repeatPassword ? (
+              <div className="text-red-500 text-sm m-1">
+                {formik.errors.repeatPassword}
+              </div>
+            ) : null}
           </div>
-          <button className="h-16 my-4 bg-slate-600 rounded-md text-white">
+
+          <button
+            type="submit"
+            className="h-16 my-8 bg-slate-600 rounded-md text-white"
+          >
             Sign Up
           </button>
         </form>
